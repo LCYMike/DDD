7a4a30c5aa126515155f3437beb4f10fd3fa4c2c
BuildToolsMikeR <mikeraadsheer11@gmail.com>
2019-09-22 02:37:24 +0200

    added PlayerFollower Barriers,

---
+++ Assets/CamFollow2d.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CamFollow2d : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 targetPosition;
+
+    public Vector2 offSet;
+
+	void FixedUpdate () {
+        targetPosition = new Vector3(target.position.x + offSet.x, target.position.y + offSet.y, transform.position.z);
+        transform.position = targetPosition;
+	}
+}
