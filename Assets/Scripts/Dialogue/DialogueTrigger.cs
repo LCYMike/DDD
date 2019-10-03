e92424a29bc2aae7feddb2d712682973283a9c1e
Mike Raadsheer <mikeraadsheer11@gmail.com>
2019-10-03 13:59:34 +0200

    functional dialogue system. extra functionality for specific situations may be needed in the future

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public string[] texts;
+
+    private float _range = 3f;
+
+    private GameObject _player;
+
+    private MeshRenderer _rend;
+    private BoxCollider _col;
+
+    private SetDialogueTxt _dialogue;
+
+    void Start()
+    {
+        _rend = GetComponent<MeshRenderer>();
+        _col = GetComponent<BoxCollider>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _dialogue = FindObjectOfType<SetDialogueTxt>();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, _player.transform.position) > _range)
+        {
+            _dialogue.abortDialogue();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _dialogue.displayText(texts);
+        _rend.enabled = false;
+        _col.enabled = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _range);
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/SetDialogueTxt.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SetDialogueTxt : MonoBehaviour
+{
+    public Action<string[]> displayText;
+    public Action abortDialogue;
+    private string[] _sentences;
+    //private List<string> _texts = new List<string>();
+
+    private float _typeSpeed = 0.15f;
+    private int index = 0;
+    private bool _isTyping = false;
+    private bool skipTyping = false;
+
+    public GameObject continueBtn;
+    public GameObject background;
+
+    private Text _txt;
+
+    void Start()
+    {
+        background.SetActive(false);
+        continueBtn.SetActive(false);
+        displayText += StartText;
+        abortDialogue += QuitDialogue;
+        _txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            skipTyping = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            NextText();
+        }
+    }
+
+    private void StartText(string[] _texts)
+    {
+        index = 0;
+        _sentences = _texts;
+        background.SetActive(true);
+        StartCoroutine(TypeText());
+    }
+
+    public void NextText()
+    {
+
+        continueBtn.SetActive(false);
+        index++;
+        Debug.Log(index + " " + _sentences.Length);
+        if (index < _sentences.Length)
+        {
+            StartCoroutine(TypeText());
+        } else
+        {
+            _txt.text = "";
+            background.SetActive(false);
+            _sentences = null;
+        }
+    }
+
+    IEnumerator TypeText()
+    {
+        _isTyping = true;
+        _txt.text = "";
+        float speed = _typeSpeed;
+        foreach (char letter in _sentences[index].ToCharArray())
+        {
+            _txt.text += letter;
+            if (skipTyping)
+            {
+                speed = 0;
+            }
+            yield return new WaitForSeconds(speed);
+        }
+        _isTyping = false;
+        skipTyping = false;
+        continueBtn.SetActive(true);
+    }
+
+    public void QuitDialogue()
+    {
+        _txt.text = "";
+        background.SetActive(false);
+        _sentences = null;
+    }
+
+}
