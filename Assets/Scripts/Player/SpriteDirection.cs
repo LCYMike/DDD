48e64a5624b5955ac7b2d8008650463798554fe3
Mike Raadsheer <mikeraadsheer11@gmail.com>
2019-11-04 15:38:24 +0100

    improved player animation and direction setter, also starting work on the enemy

---
+++ Assets/Scripts/Player/SpriteDirection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteDirection : MonoBehaviour
+{
+
+    private PlayerMovement2D _mov;
+
+    public SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        _mov = GetComponent<PlayerMovement2D>();
+        _mov.playerDir += SetX;
+    }
+
+
+    void SetX(float _dir)
+    {
+        if(_dir < 0)
+        {
+            rend.flipX = true;
+        }
+        else if(_dir > 0)
+        {
+            rend.flipX = false;
+        }
+    }
+}
